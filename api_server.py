71131dd0cf3eba6b1244ca3be6e67cd31a79d942
Sara Bonnin Richaud <saesbori@gmail.com>
2022-09-11 14:29:14 +0100

    static_files directory for Flask static_folder assignation to allow CSS application when using Flask render_templates

--- api_server.py
+++ api_server.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import webbrowser
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static_files')
 
+
+# ROUTES TO EACH WEBPAGE
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/genre")
+def genre():
+    return render_template("genre.html")
+
+
+@app.route("/decade")
+def decade():
+    return render_template("decade.html")
+
+
+@app.route("/movie")
+def movie():
+    return render_template("movie.html")
+
+
+@app.route("/streaming")
+def streaming():
+    return render_template("streaming.html")
+
+
+# ->TO CONTINUE WITH TUTORIAL: https://www.youtube.com/watch?v=9MHYHgh4jYc
+
 
 ### GENRE USER SELECTION
 
@@ -23,4 +49,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)  # debug=True avoids having to re run server every time after making changes
